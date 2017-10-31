8239a88a6bfa391100bbf9e7839fd85a6601d8c5
Victor J. Marin <vxm4964@rit.edu>
2017-10-31 16:49:42 -0400

    Simplify test

--- src/assignment4/SchedulingTest.java
+++ src/assignment4/SchedulingTest.java
@@ -1,6 +1,5 @@
 package assignment4;
 
-import java.util.concurrent.Future;
 import assignment4.pooling.PriorityCallable;
 import assignment4.pooling.PriorityThreadPoolExecutor;
 
@@ -14,13 +13,8 @@ public class SchedulingTest {
 
       @Override
       public Integer call() {
-        Integer partialSum = 0;
-        final int low = 0;
-        final int hi = 100;
-        for (int i = low; i <= hi; i++) {
-          partialSum += i;
-        }
-        return partialSum;
+        System.out.println("Running lower priority task.");
+        return null;
       }
 
       @Override
@@ -33,13 +27,8 @@ public class SchedulingTest {
 
       @Override
       public Integer call() {
-        Integer partialSum = 0;
-        final int low = 1;
-        final int hi = 9000000;
-        for (int j = low; j <= hi; j++) {
-          partialSum += (int) Math.log10(partialSum);
-        }
-        return partialSum;
+        System.out.println("Running higher priority task.");
+        return null;
       }
 
       @Override
@@ -49,36 +38,21 @@ public class SchedulingTest {
     };
 
 
-    // Two lowerPriorityTask (f1 and f2) are submitted before a higherPriorityTask (f3).
-    // Since we only have 1 thread available, the first submitted task (f1) will be run immediately,
-    // and the other two will be executed based on their priority. Even though f2 is submitted
-    // before f3, f3 has a higher priority and will be run before f2. Therefore, the output in
+    // Two lowerPriorityTask (t1 and t2) are submitted before a higherPriorityTask (t3).
+    // Since we only have 1 thread available, the first submitted task (t1) will be run immediately,
+    // and the other two will be executed based on their priority. Even though t2 is submitted
+    // before t3, t3 has a higher priority and will be run before t2. Therefore, the output in
     // console should be:
-    // f1 done.
-    // f3 done.
-    // f2 done.
-    final Future<Integer> f1 = threadPool.submit(lowerPriorityTask);
-    final Future<Integer> f2 = threadPool.submit(lowerPriorityTask);
-    final Future<Integer> f3 = threadPool.submit(higherPriorityTask);
+    // Running lower priority task.
+    // Running higher priority task.
+    // Running lower priority task.
 
-    boolean f1Done = false;
-    boolean f2Done = false;
-    boolean f3Done = false;
-
-    while (!f1Done || !f2Done || !f3Done) {
-      if (f1.isDone() && !f1Done) {
-        f1Done = true;
-        System.out.println("f1 done.");
-      }
-      if (f2.isDone() && !f2Done) {
-        f2Done = true;
-        System.out.println("f2 done.");
-      }
-      if (f3.isDone() && !f3Done) {
-        f3Done = true;
-        System.out.println("f3 done.");
-      }
-    }
+    // t1
+    threadPool.submit(lowerPriorityTask);
+    // t2
+    threadPool.submit(lowerPriorityTask);
+    // t3
+    threadPool.submit(higherPriorityTask);
 
     threadPool.shutdown();
 
